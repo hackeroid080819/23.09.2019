3bcc407e222c9087317d43e1d28934daac3febaf
hackeroid080819 <53891847+hackeroid080819@users.noreply.github.com>
2019-09-23 11:37:43 +0300

    Add files via upload

---
+++ ProjectStarter/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.itaykan.flightcenter;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.FirebaseDatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+    ArrayAdapter adapter;
+    ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        // change the menu icon to something else...
+        Toolbar t = findViewById(R.id.toolbar);
+        t.setOverflowIcon(ContextCompat.getDrawable(getApplicationContext(),
+                android.R.drawable.btn_star));
+
+        lv = findViewById(R.id.flightLV);
+        adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, new ArrayList<String>() );
+        lv.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
